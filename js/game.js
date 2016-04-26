16e6a030eeaa67b53906d4f6913bed40fc56eb56
Andrew Lombardi <andrew@mysticcoders.com>
2016-04-26 15:49:38 -0700

    fix to remove some of the drawing

--- js/game.js
+++ js/game.js
@@ -1,14 +1,12 @@
 var gamelib = new GameLib(512, 480);
 
-gamelib.setBackground("images/background.png");
-
-gamelib.registerSprite("hero", new Sprite("images/hero.png"));
-
-var hero = gamelib.getSprite("hero");
+// gamelib.setBackground("images/background.png");
+// gamelib.registerSprite("hero", new Sprite("images/hero.png"));
+// var hero = gamelib.getSprite("hero");
 
 // Reset the game when the player catches a monster
 var reset = function () {
-	hero.moveCenter(gamelib.canvas);
+	// hero.moveCenter(gamelib.canvas);
 };
 
 // Update game objects
